8d2615d8118ce314ad87b18b4742bc1567d12293
Luisotb <jluisoterobarreiro@gmail.com>
2021-06-14 11:23:02 +0200

    commito

--- src/main/java/ExamenEntornos/Controllers/GuardaBaseDatos.java
+++ src/main/java/ExamenEntornos/Controllers/GuardaBaseDatos.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GuardaBaseDatos {
 
   @RequestMapping("/save")
-  public String insercionDatos() {
-    return "insercionDatos";
+  public String save() {
+    return "save";
   }
 }
